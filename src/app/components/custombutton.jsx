46712466cf8d4ff752f3fb6ae2fa8168b5b4c825
PlmFerraz <140653429+PlmFerraz@users.noreply.github.com>
2025-04-24 14:35:16 -0300

    Feat. criação do botão customizável

--- src/app/components/custombutton.jsx
+++ src/app/components/custombutton.jsx
@@ -1,3 +1,34 @@
-export default function CustomButton({ children }) {
-  return <button className="text-blue-500">{children}</button>;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faGithub,
+  faLinkedin,
+  faChrome,
+} from "@fortawesome/free-brands-svg-icons";
+
+export default function CustomButton({ children, iconName, url }) {
+  const getIcon = (iconName) => {
+    switch (iconName) {
+      case "instagram":
+        return faInstagram;
+      case "github":
+        return faGithub;
+      case "linkedin":
+        return faLinkedin;
+      default:
+        return faChrome;
+    }
+  };
+
+  return (
+    <button className="bg-[#333333] rounded-lg">
+      <a
+        className="w-full p-[10px]  flex justify-center items-center gap-2"
+        href={url}
+      >
+        <FontAwesomeIcon icon={getIcon(iconName)} />
+        {children}
+      </a>
+    </button>
+  );
 }
