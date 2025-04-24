0ca9a17b56950683cc909acaff4b21d4305696fc
PlmFerraz <140653429+PlmFerraz@users.noreply.github.com>
2025-04-24 14:30:52 -0300

    Feat. criação do arquivo mock

---
+++ src/mocks/user-mock.js
@@ -0,0 +1,10 @@
+export const userMock = {
+  userName: "Paloma Ferraz",
+  urlGitHub: "https://github.com/PlmFerraz",
+  urlInstagram: "https://www.instagram.com/plmferraz/",
+  urlLinkedin: "https://www.linkedin.com/in/paloma-ferraz-7b6555141/",
+  urlAvatar:
+    "https://media.licdn.com/dms/image/v2/D4D03AQGu2Xzvs4Hg6Q/profile-displayphoto-shrink_800_800/B4DZQKsOFsHUAc-/0/1735346152214?e=1750896000&v=beta&t=VGjwenvVJ7WdEhKKdMBvz7Af1k8z4cuOdIAEO3t-P78",
+  location: "Rio Grande do Sul, Brasil",
+  jobDescription: "Front-end developer and reader.",
+};
